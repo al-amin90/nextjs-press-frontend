56260e93f24156b666a0085c63a25e694417802a
al-amin90 <ijesun30@gmail.com>
2026-07-15 18:32:56 +0600

    feat: Part1

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-const BlogPage = () => {
+const BlogPage = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const res = await data.json();
+  console.log("res", res);
   return <div>Pafw</div>;
 };
 
