137a5b965547ba6c0d395c20906b5f97d7720993
al-amin90 <ijesun30@gmail.com>
2026-07-21 16:03:58 +0600

    feat: Project Route Structuring Of Our First Frontend Project

--- app/blogs/[slug]/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const loading = () => {
-  return <div>losinf ...</div>;
-};
-
-export default loading;
--- app/blogs/authors/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AuthorsPage = () => {
-  return <div>AuthorsPage</div>;
-};
-
-export default AuthorsPage;
