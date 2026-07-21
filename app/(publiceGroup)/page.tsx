ee2a8289a659fcf0a02fec40bfbc0cb97b0a4839
al-amin90 <ijesun30@gmail.com>
2026-07-21 16:41:42 +0600

    feat: Integrating ShadcnU

--- app/(publiceGroup)/page.tsx
+++ app/(publiceGroup)/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import LikeBtn from "./ui/LikeBtn";
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -18,7 +16,6 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.{" "}
             {/* <Link href={"/blogs"}>Blogs</Link> */}
-            <LikeBtn />
           </h1>
         </div>
       </main>
