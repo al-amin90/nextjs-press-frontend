22f2acc52e0018e6befe0c13e19f2da00aecdefd
al-amin90 <ijesun30@gmail.com>
2026-07-22 16:49:08 +0600

    feat: [200~Handling Auth Routes In Proxy File

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,40 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const AUTH_ROUTES = ["/login", "register"];
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
+
+  //   const cookieStore = await cookies();
+  //   const accessToken = cookieStore.get("cookieStore")?.value;
+
+  const accessToken = request.cookies.get("cookieStore")?.value;
+
+  const decodedToken = accessToken
+    ? (jwt.decode(accessToken) as JwtPayload)
+    : null;
+
+  let userRole = null;
+
+  if (decodedToken) {
+    userRole = decodedToken.role;
+  }
+
+  if (accessToken && AUTH_ROUTES.includes(pathName)) {
+    if (userRole === "USER") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else if (userRole === "AUTHOR") {
+      return NextResponse.redirect(new URL("/author-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
 
   console.log("rew", request);
   return NextResponse.redirect(new URL("/", request.url));
