8408acc5ce2b715a3f9b587d3aaeea670a25d6e0
al-amin90 <ijesun30@gmail.com>
2026-07-22 18:06:00 +0600

    feat: Authorizing Role Based Protected Routes In Proxy

--- proxy.ts
+++ proxy.ts
@@ -2,27 +2,41 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { jwtUtils } from "./utils/jwt";
 
-const AUTH_ROUTES = ["/login", "register"];
+const AUTH_ROUTES = ["/login", "/register"];
+const PUBLIC_ROUTES = ["/", "news", "/login", "/register"];
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
 
-  //   const cookieStore = await cookies();
-  //   const accessToken = cookieStore.get("cookieStore")?.value;
+  console.log("proxy", pathName);
 
-  const accessToken = request.cookies.get("cookieStore")?.value;
+  const cookieStore = await cookies();
+  //   const accessToken = cookieStore.get("accessToken")?.value;
+
+  const accessToken = request.cookies.get("accessToken")?.value;
 
   const decodedToken = accessToken
-    ? (jwt.decode(accessToken) as JwtPayload)
+    ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_TOKEN as string)
     : null;
+
+  console.log("decodedToken", decodedToken);
 
   let userRole = null;
 
-  if (decodedToken) {
-    userRole = decodedToken.role;
+  if (!decodedToken?.success) {
+    // token has expired
+    cookieStore.delete("accessToken");
+    return NextResponse.redirect(new URL("/login", request.url));
   }
+
+  if (decodedToken?.success && decodedToken.data) {
+    userRole = (decodedToken.data as JwtPayload).role;
+  }
+
+  console.log("userRole", userRole);
 
   if (accessToken && AUTH_ROUTES.includes(pathName)) {
     if (userRole === "USER") {
@@ -36,8 +50,29 @@ export async function proxy(request: NextRequest) {
     }
   }
 
-  console.log("rew", request);
-  return NextResponse.redirect(new URL("/", request.url));
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => route === pathName || pathName.startsWith(route + "/"),
+  );
+
+  if (!isPublicRoute && !accessToken) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  //   authorization
+  if (pathName.startsWith("/dashboard") && userRole !== "USER") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (pathName.startsWith("/admin-dashboard") && userRole === "ADMIN") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (
+    pathName.startsWith("/author-dashboard") &&
+    userRole === "AUTHOR"
+  ) {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  }
+
+  return NextResponse.next();
+
+  //   return NextResponse.redirect(new URL("/", request.url));
 }
 
 // Alternatively, you can use a default export:
@@ -45,8 +80,10 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/dashboard/:path*",
-    "/admin-dashboard/:path*",
-    "/author-dashboard/:path*",
+    // "/dashboard/:path*",
+    // "/admin-dashboard/:path*",
+    // "/author-dashboard/:path*",
+
+    "/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)",
   ],
 };
---
+++ utils/jwt.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import jwt from "jsonwebtoken";
+
+const verifyToken = (token: string, secret: string) => {
+  try {
+    const verifiedToken = jwt.verify(token, secret);
+
+    return {
+      success: false,
+      data: verifiedToken,
+    };
+  } catch (error: any) {
+    console.log("Invalid Token", error);
+    return {
+      success: false,
+      error: error.message,
+    };
+  }
+};
+
+export const jwtUtils = {
+  verifyToken,
+};
